add1a44db1c489f5671d174f485cd9545790c600
Nate Bierdeman <nbierdeman@paypal.com>
2021-08-13 11:16:33 -0500

    Add program introduction

---
+++ charfreq.js
@@ -0,0 +1,8 @@
+/*
+This Node program reads text from standard input, computes the frequency
+of each letter in that text, and displays a histogram of the most
+frequently used characters. It require Node 12 or higher to run.
+
+In a Unix-type environment, you can invoke the program like this:
+  node charfreq.js < corpus.txt
+*/
